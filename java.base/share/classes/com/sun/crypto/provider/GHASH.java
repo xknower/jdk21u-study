7902c907dbe3bb664d97ac3396758e0cffb9fddd
xknower <xknower@126.com>
2023-12-01 18:26:33 +0800

    全局格式化与代码规范处理

--- java.base/share/classes/com/sun/crypto/provider/GHASH.java
+++ java.base/share/classes/com/sun/crypto/provider/GHASH.java
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2013, 2021, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-/*
- * (C) Copyright IBM Corp. 2013
- * Copyright (c) 2015 Red Hat, Inc.
- */
-
 package com.sun.crypto.provider;
 
 import java.lang.invoke.MethodHandles;
@@ -47,7 +18,6 @@ import jdk.internal.vm.annotation.IntrinsicCandidate;
  *
  * @since 1.8
  */
-
 final class GHASH implements Cloneable, GCM {
 
     private static final int AES_BLOCK_SIZE = 16;
--- java.base/share/classes/sun/nio/cs/UTF_8.java
+++ java.base/share/classes/sun/nio/cs/UTF_8.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package sun.nio.cs;
 
 import jdk.internal.access.JavaLangAccess;
@@ -37,7 +12,8 @@ import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CoderResult;
 import java.nio.charset.CodingErrorAction;
 
-/* Legal UTF-8 Byte Sequences
+/**
+ * Legal UTF-8 Byte Sequences
  *
  * #    Code Points      Bits   Bit/Byte pattern
  * 1                     7      0xxxxxxx
@@ -54,9 +30,7 @@ import java.nio.charset.CodingErrorAction;
  *     U+10000..U+3FFFF         F0          90..BF      80..BF      80..BF
  *     U+40000..U+FFFFF         F1..F3      80..BF      80..BF      80..BF
  *    U+100000..U10FFFF         F4          80..8F      80..BF      80..BF
- *
  */
-
 public final class UTF_8 extends Unicode {
 
     public static final UTF_8 INSTANCE = new UTF_8();
