30e6bc7dbbf1b560186990e807fc93c51f0a9ace
xknower <xknower@126.com>
2023-12-02 03:32:31 +0800

    全局格式化与代码规范处理

--- utils/LogCompilation/src/main/java/com/sun/hotspot/tools/compiler/LogParser.java
+++ utils/LogCompilation/src/main/java/com/sun/hotspot/tools/compiler/LogParser.java
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2009, 2021, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- *
- */
-
-/**
- * A SAX based parser of LogCompilation output from HotSpot.  It takes a complete
- */
-
 package com.sun.hotspot.tools.compiler;
 
 import java.io.FileReader;
@@ -49,6 +21,8 @@ import org.xml.sax.Locator;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
+ * A SAX based parser of LogCompilation output from HotSpot.  It takes a complete
+ *
  * A SAX parser for HotSpot compilation logs. The bulk of the parsing and event
  * maintenance work is done in the {@link #startElement(String,String,String,Attributes)}
  * and {@link #endElement(String,String,String)} methods.
